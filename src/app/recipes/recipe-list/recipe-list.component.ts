e3b3618fc09f514a20db60dd1740a94ec237dc99
quanghotrungthinh <quanghotrungthinh@gmail.com>
2023-05-04 21:28:48 +0700

    12. Displaying Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'this is simple test', 'https://cdn.onshop.asia/images/kennymarket/9ea5eeddfc8901d75898.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
